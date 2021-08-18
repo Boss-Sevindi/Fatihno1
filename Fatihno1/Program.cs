63ccf0b3361f91485c1e582a2df73492a381b990
Fatih Sevindi <f.sevindi@gso.schule.koeln>
2021-08-18 15:01:47 +0200

    Ausgabe Welt ergänzt

--- Fatihno1/Program.cs
+++ Fatihno1/Program.cs
@@ -7,7 +7,8 @@ namespace Fatihno1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");//Textausgabe
-        
+            Console.WriteLine("Welt");
+
         }
     }
 }
